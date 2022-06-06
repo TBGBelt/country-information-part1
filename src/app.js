e7d2d3fd13fd400bc59b9d4e00f024a2727324ee
TBGBelt <tbgbelt@gmail.com>
2022-06-06 17:51:41 +0200

    added styling

--- src/app.js
+++ src/app.js
@@ -18,8 +18,11 @@ async function fetchDataFromCountryApi() {
     try {
         const response = await axios.get(`https://restcountries.com/v2/all`);
         console.log(response.data)
-        const countries = response.data
+        const countries = response.data;
         // const responseAddedInHTML = document.getElementById("all-countries");
+        countries.sort((low, high) => {
+            return low.population - high.population;
+        });
         countriesList(countries);
 
     } catch (e) {
@@ -31,12 +34,31 @@ fetchDataFromCountryApi();
 function countriesList(countries) {
     let responseAddedInHTML = document.getElementById("all-countries")
     responseAddedInHTML.innerHTML = countries.map((country)=>{
-        return `<li>
-            <h2>${country.name}</h2>
-            <p>has a population of ${country.population} people</p>
-            <img src="${country.flags.png}" alt="flag of ${country.name}">
+        return `
+        <li>
+            <h2 class="${changeColorCountry(country.region)}">${country.name}</h2>
+            <img src="${country.flags.png}" alt="flag of ${country.name}" class="flags">
+            <p class="population">has a population of ${country.population} people</p>
         </li>`;
     }).join(" ")
+}
+
+function changeColorCountry(region) {
+    switch (region) {
+        case "Asia":
+            return 'asia';
+        case "Europe":
+            return 'europa';
+        case "Afrika":
+            return 'afrika';
+        case "Americas":
+            return 'americas'
+        case "Oceania":
+            return 'oceania';
+        default:
+            return "otherCountries";
+
+    }
 }
 
 // async function fetchDataRestCountriesApi() {
